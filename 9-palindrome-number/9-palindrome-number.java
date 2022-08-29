7058f6559f85afd49dc7db85966a9970e9a21683
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-08-29 19:23:00 +0200

    Time: 85 ms (5.07%), Space: 51.5 MB (5.50%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -4,7 +4,8 @@ class Solution {
             return false;
         else if(x < 10)
             return true;
-        
+        // 20005   50002  (20005 / (10^(5 length - 1)) = 2.0005 (int 2))
+        // 20005   50002  (20005 / 10^(5-2)) = 20
         String value = String.valueOf(x);
         String palindrome = "";
         for(int i = value.length() - 1; i >= 0; i--)
