a72e698f818344d63d1dc1cd875545714b1744bd
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-09-05 05:11:50 +0200

    Time: 28 ms (5.02%), Space: 43.4 MB (6.38%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() % 2 == 1)
+            return false;
+        
+        Stack<Character> stack = new Stack<Character>();
+        
+        for(int i =0; i < s.length(); i++)
+        {
+            Character chr = s.charAt(i);
+            if(chr == '(' || chr == '{' || chr == '[') stack.push(chr);
+            else{
+                if(stack.isEmpty()) return false; // Expecting Right Closing Bracket Not Empty
+     else{
+         Character top = stack.peek();
+ if((top == '(' && chr == ')') || (top == '{'  && chr == '}') || (top == '[' &&  chr == ']')) {
+                        System.out.println("Open Tag: " + top + " & Closing Tag: " + chr );
+                stack.pop();
+      }else return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+        }
+}
