9e047a99c8c20713992b2fcffd0adf0f458e0050
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-09-06 08:59:03 +0200

    Time: 0 ms (100.00%), Space: 40.3 MB (95.62%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -2,7 +2,7 @@ class Solution {
     public int lengthOfLastWord(String s) {
         int i = s.length() - 1;
         boolean isWordFound = false;
-        int lengthCount = 0;
+    /*    int lengthCount = 0;
         while (i >= 0 )
         {
             if(s.charAt(i) != ' ')
@@ -12,8 +12,29 @@ class Solution {
             }else if(isWordFound && s.charAt(i) == ' ')
             { return lengthCount;}
             i--;
+        }*/
+        
+        
+        while (i >= 0 )
+        {
+            if(s.charAt(i) != ' ')
+            {
+                isWordFound = true;
+                int j = i;
+                int lengthCount = 0;
+                while(j >= 0 && j <= i)
+                {
+                    lengthCount++;
+                    if(s.charAt(j) == ' ' ) return (i - j);
+                    j--;
+                }
+                return lengthCount;
+                
+            }
+            i--;
         }
-        return lengthCount;
+        
+        return -1;
             
     }
 }
