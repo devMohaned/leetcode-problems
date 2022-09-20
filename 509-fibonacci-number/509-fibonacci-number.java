63504700c186c476c29e1923c2fd0ec3e5747e2d
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-09-20 10:10:51 +0200

    Time: 0 ms (100.00%), Space: 41.1 MB (33.15%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,11 +1,19 @@
 class Solution {
     public int fib(int value) {
-        if(value == 0 ||value == 1)
+        if(value == 0 || value == 1)
             return value;
-        if(value == 2)
-        return 1;
-                        
-        return fib(value - 1) + fib(value - 2);
+        
+       int[] lastTwo = new int[2];
+        lastTwo[0] = 0;
+        lastTwo[1] = 1;
+        int count = 2;
+        while(count <= value){
+            int next = lastTwo[0] + lastTwo[1];
+            lastTwo[0] = lastTwo[1];
+            lastTwo[1] = next;
+            count++;
+        }
+        return value > 1 ? lastTwo[1] : lastTwo[0];
     }
     
 
