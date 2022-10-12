01a27377eada142ca640e3de751f0777e2a9ff79
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-10-12 03:19:04 +0200

    Time: 18 ms (31.44%), Space: 44 MB (80.73%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -4,20 +4,9 @@ class Solution {
             return false;
         if(x < 10)
             return true;
-        
-        
-        String numberInString = String.valueOf(x);
-        String palindrom = "";
-        for(int i = numberInString.length() - 1 ; i >= 0; i--)
-        {
-            char c = numberInString.charAt(i);
-            palindrom += c;
-        }
-        
-        return palindrom.equals(numberInString);
             
         
-     /*   int palindrom = 0;
+        int palindrom = 0;
         int temp = x;
         while(temp > 0)
         {
@@ -26,7 +15,7 @@ class Solution {
             temp = temp / 10;
         }
         
-        return palindrom == x;*/
+        return palindrom == x;
         
         
         
@@ -58,42 +47,6 @@ class Solution {
         
         
         
-        
-        
-        
-        
-      /*  if(x < 0)
-            return false;
-        else if(x < 10)
-            return true;
-        */
-      /*  String value = String.valueOf(x);
-        String palindrome = "";
-        for(int i = value.length() - 1; i >= 0; i--)
-        {
-            char c = value.charAt(i);
-            palindrome += c;
-        }
-        if(palindrome.equals(value))
-            return true;
-        
-        return false;*/
-        
-      /*  int temp = x;
-        int reverse = 0;
-        while(temp > 0)
-        {
-            int lastDigit = temp % 10; // 1
-            reverse = (reverse*10) + lastDigit; // 32*10 + 1
-            temp = temp / 10; // 0
-        }
-        
-        if(x == reverse)
-            return true;
-        else
-            return false;
-        
-        */
         
     }
 }
