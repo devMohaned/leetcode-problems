a477dc96a722db5ee248baee17fbda3cc46b25cb
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-10-14 07:33:26 +0200

    Time: 0 ms (100.00%), Space: 42 MB (97.27%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,6 +1,67 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int[] temp = nums1.clone();
+        int leftIdx = 0;
+        int rightIdx = 0;
+        int toBeAddedIdx = 0;
+        while(leftIdx < m && rightIdx < n)
+        {
+            if(temp[leftIdx] < nums2[rightIdx])
+                nums1[toBeAddedIdx++] = temp[leftIdx++];
+            else  
+                nums1[toBeAddedIdx++] = nums2[rightIdx++];
+            
+        }
+        // add remaining elements
+        if(leftIdx < m)
+        {
+            for(int i = leftIdx; i < m; i++)
+            {
+                nums1[toBeAddedIdx++] = temp[leftIdx++];
+            }
+        }
+        
+         if(rightIdx < n)
+        {
+            for(int i = rightIdx; i < n; i++)
+            {
+                nums1[toBeAddedIdx++] = nums2[rightIdx++];
+            }
+        }
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      /*  int[] temp = nums1.clone();
         int firstIdx = 0;
         int secondIdx = 0;
         int insertedIdx = 0;
@@ -29,9 +90,9 @@ class Solution {
         {
             for(int i = secondIdx; i < n; i++)
             {
-                    int remaining = nums2[i];
-  nums1[insertedIdx++] = remaining;
+                int remaining = nums2[i];
+                nums1[insertedIdx++] = remaining;
             }
         }
-    }
+    }*/
 }
