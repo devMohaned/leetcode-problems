56f984bb3d4eefa557ee20b50e3a6900bfba7af6
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-10-22 03:47:26 +0200

    Time: 0 ms (100.00%), Space: 51.2 MB (71.64%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -17,6 +17,30 @@ class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         while(root != null)
         {
+            if(val > root.val)
+            {
+                root = root.right;
+            }else if(val < root.val)
+            {
+                root = root.left;
+            }else {
+                return root;
+            }
+        }
+        
+        return root;
+
+        
+        
+        
+    }
+    
+    
+    
+    
+    /*
+    while(root != null)
+        {
             if(val < root.val)
             {
                 root = root.left;
@@ -28,6 +52,6 @@ class Solution {
             }
         }
                         return root;
-
-    }
+                        */
+    
 }
