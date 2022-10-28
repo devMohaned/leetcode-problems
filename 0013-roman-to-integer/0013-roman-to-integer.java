34d119e9ebb6d632b18ee7b494c120a60605e9df
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-10-28 08:54:36 +0200

    Time: 18 ms (16.17%), Space: 50.5 MB (7.31%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int romanToInt(String s) {
+        if(s.equals("IV"))
+            return 4;
+        else if(s.equals("IX"))
+            return 9;
+        else if(s.equals("XL"))
+            return 40;
+         else if(s.equals("XC"))
+            return 90;
+         else if(s.equals("CD"))
+            return 400;
+         else if(s.equals("CM"))
+            return 900;
+        
+        Map<String, Integer> roman = new HashMap<String, Integer>();
+        roman.put("I", 1);
+        roman.put("V", 5);
+        roman.put("X", 10);
+        roman.put("L", 50);
+        roman.put("C", 100);
+        roman.put("D", 500);
+        roman.put("M", 1000);
+        roman.put("IV", 4);
+        roman.put("IX", 9);
+        roman.put("XL", 40);
+        roman.put("XC", 90);
+        roman.put("CD", 400);
+        roman.put("CM", 900);
+        
+        int N = s.length();
+        int result = 0;
+        for(int i = 0; i < N;i++)
+        {
+            String subset = "";
+            if(i + 2 <=  N) 
+            subset = s.substring(i, i + 2);            
+                
+            if(roman.containsKey(subset) && !subset.equals(""))
+            {
+                result += roman.get(subset);
+                i++;
+            }else{
+                subset = s.substring(i, i + 1);
+                result += roman.get(subset);
+                }
+        }
+        
+        
+        return result;
+        
+    }
+}
