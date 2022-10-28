070d511f65ec775310a2e0e0952cb113faf72d5f
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-10-28 07:27:16 +0200

    Time: 5 ms (48.34%), Space: 50 MB (38.14%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -7,8 +7,13 @@ class Solution {
        
         int M = nums1.length;
         int N = nums2.length;
+        if(M == 0)
+           return findMedian(nums2,N);
+        if(N == 0)
+        return findMedian(nums1,M);
+
         int T = M+N;
-  int leftIdx = 0;
+        int leftIdx = 0;
         int rightIdx = 0;
         int highest = Math.max(M,N);
         double[] merged = new double[M+N];
@@ -35,9 +40,17 @@ class Solution {
             }
                 i++;
 }
-        if(T % 2 == 0)   return (merged[((T/2) - 1)] + merged[T/2])/2;
-        else return merged[((T+1)/2) - 1];
+       return findMedian(merged, T);
         
               
+    }
+    
+    private double findMedian(double[] merged, int T){
+        return T % 2 == 0 ?(merged[((T/2) - 1)] + merged[T/2])/2 
+            : merged[((T+1)/2) - 1];
+    }
+      private double findMedian(int[] merged, int T){
+        return T % 2 == 0 ?((double)(merged[((T/2) - 1)] + merged[T/2])/2) 
+            : ((double)merged[((T+1)/2) - 1]);
     }
 }
