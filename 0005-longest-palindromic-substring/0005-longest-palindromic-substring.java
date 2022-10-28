3ca17f69b4ea65867da4e1a6113c84eeef943e71
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-10-28 05:06:49 +0200

    Time: 38 ms (61.45%), Space: 46.1 MB (44.88%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -15,7 +15,7 @@ class Solution {
         }
         
         
-        return s.substring(currentLongestIdx[0],currentLongestIdx[1]); // 2nd Parameter is exclusive (i.e: 'ABCD' -> substring(0,3) = 'ABC')
+        return s.substring(currentLongestIdx[0],currentLongestIdx[1]);
     }
     
     private int[] getLongestPalindromeIdx(String s, int leftIdx, int rightIdx)
@@ -28,7 +28,7 @@ class Solution {
                 leftIdx--;
                 rightIdx++;
             }
-            return new int[]{leftIdx + 1, rightIdx}; // (2nd Parameter should be - 1, but due to the influence of substring's 2nd parameter, which is exclusive)
+            return new int[]{leftIdx + 1, rightIdx};
     }
     
     private int[] updateLongestOrKeep(int[] arr, int[] oddOrEven){
