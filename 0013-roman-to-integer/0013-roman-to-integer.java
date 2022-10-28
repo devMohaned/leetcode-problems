94f4707efa230f8c5a4d8839ecb07e22be44ab4e
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-10-28 09:17:10 +0200

    Time: 6 ms (90.37%), Space: 45.2 MB (67.43%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,53 +1,33 @@
 class Solution {
     public int romanToInt(String s) {
-        if(s.equals("IV"))
-            return 4;
-        else if(s.equals("IX"))
-            return 9;
-        else if(s.equals("XL"))
-            return 40;
-         else if(s.equals("XC"))
-            return 90;
-         else if(s.equals("CD"))
-            return 400;
-         else if(s.equals("CM"))
-            return 900;
-        
-        Map<String, Integer> roman = new HashMap<String, Integer>();
-        roman.put("I", 1);
-        roman.put("V", 5);
-        roman.put("X", 10);
-        roman.put("L", 50);
-        roman.put("C", 100);
-        roman.put("D", 500);
-        roman.put("M", 1000);
-        roman.put("IV", 4);
-        roman.put("IX", 9);
-        roman.put("XL", 40);
-        roman.put("XC", 90);
-        roman.put("CD", 400);
-        roman.put("CM", 900);
-        
-        int N = s.length();
-        int result = 0;
-        for(int i = 0; i < N;i++)
-        {
-            String subset = "";
-            if(i + 2 <=  N) 
-            subset = s.substring(i, i + 2);            
-                
-            if(!subset.equals("")&& roman.containsKey(subset) )
-            {
-                result += roman.get(subset);
-                i++;
-            }else{
-                subset = s.substring(i, i + 1);
-                result += roman.get(subset);
-                }
+        // Answer Result / Current Number / Previous Number
+    int answer = 0, number = 0, prev = 0;
+
+        // Solving Backward
+    for (int j = s.length() - 1; j >= 0; j--) {
+        switch (s.charAt(j)) {
+            case 'M': number = 1000; break;
+            case 'D': number = 500; break;
+            case 'C': number = 100; break;
+            case 'L': number = 50; break;
+            case 'X': number = 10; break;
+            case 'V': number = 5; break;
+            case 'I': number = 1; break;
+        }
+
+           // If the current number is smaller than the previous, thus, we decrement the answer by the current number
+ if (number < prev) {
+            answer -= number; 
+        }
+        else { 
+    // If the current number is greater or equal, thus, we increment the answer by the current number
+            
+            answer += number;
         }
         
-        
-        return result;
+        prev = number;
+    }
+    return answer;
         
     }
 }
