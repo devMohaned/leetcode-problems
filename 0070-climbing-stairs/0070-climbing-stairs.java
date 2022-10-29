c28856135bc161ad745d0859c552085e4e491a2b
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-10-29 14:14:07 +0200

    Time: 0 ms (100.00%), Space: 41.1 MB (22.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,28 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n <= 3)
+      /*  if(n <= 3)
             return n;
       
         
         int a = 3;
         int b = 2;
-        for(int i =0; i < n - 3; i++)
+        for(int i = 0; i < n - 3; i++)
         {
-         a = a + b;
-            b = a - b;
+        a = a + b;
+        b = a - b;
         }
-        return a;
+        return a;*/
+        
+        int prev = 0;
+        int current = 1;
+        int temp = 0;
+        
+        for(int i =0; i < n; i++)
+        {
+            temp = current;
+            current = current + prev;
+            prev = temp;
+        }
+        return current;
     }
 }
