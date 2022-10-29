277701c72f9ef50b665bb45c95020fe811d8c2b1
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-10-29 07:49:00 +0200

    Time: 1 ms (96.81%), Space: 42.8 MB (51.34%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,36 @@
+class Solution {
+   	StringBuilder  output  = new StringBuilder();
+    String[] mapping = 
+    {"" , "" , "abc" ,"def" ,"ghi" ,"jkl", "mno" ,"pqrs" ,"tuv" ,"wxyz"};
+	int index = 0;
+
+    public List<String> letterCombinations(String digits) {
+      ArrayList<String> ans = new ArrayList<>();
+
+		if(digits.length() == 0) return ans;
+        else helper(digits , output , index , ans , mapping);
+		return ans;
+
+	}
+
+	private static void helper
+(String digits, StringBuilder output, int index, ArrayList<String> ans,
+ String[] mapping){
+		// base case 
+		if(output.toString().length()  == digits.length()){
+			String base = output.toString();
+			ans.add(base);
+			return;
+		}
+
+		int ch = digits.charAt(index) - '0';
+		String val = mapping[ch];
+		for(int i = 0 ; i < val.length() ; i++){
+			output.append(val.charAt(i));
+			helper(digits,output , index + 1  , ans , mapping);
+			output.deleteCharAt(output.length() - 1);
+		}
+
+	}
+ 
+}
