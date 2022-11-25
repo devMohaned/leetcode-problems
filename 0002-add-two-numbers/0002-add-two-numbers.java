4254643b8d46445213cbcd83c696a4570268cdd9
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2022-11-25 15:07:05 +0200

    Time: 2 ms (99.19%), Space: 42.3 MB (92.02%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,42 +10,45 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode temp = null;
-        ListNode head = null;
-        
-        int sum = 0;
+ ListNode head = null;
+        ListNode temp = null;
+        // Carry
         int carry = 0;
-        while(l1 != null || l2 != null)
-        {
-            sum = carry;
-            if(l1 != null)
-            {
+        // Loop for the two lists
+        while (l1 != null || l2 != null) {
+            // At the start of each iteration, we should add carry from the last iteration
+            int sum = carry;
+            // Since the lengths of the lists may be unequal, we are checking if the
+            // current node is null for one of the lists
+            if (l1 != null) {
                 sum += l1.val;
                 l1 = l1.next;
             }
-            
-            if(l2 != null)
-            {
+            if (l2 != null) {
                 sum += l2.val;
                 l2 = l2.next;
             }
-            
+            // At this point, we will add the total remaindar sum % 10 to the new node
+            // in the resultant list
+            ListNode node = new ListNode(sum % 10);
+            // Carry to be added in the next iteration
             carry = sum / 10;
-            ListNode newNode = new ListNode(sum % 10);
-            
-            if(temp == null)
-            {
-                temp = head = newNode;
-            }else{
-                temp.next = newNode;
+            // If this is the first node or head
+            if (temp == null) {
+                temp = head = node;
+            }
+            // For any other node
+            else {
+                temp.next = node;
                 temp = temp.next;
             }
         }
-        
-        if(carry > 0)
-        {
+        // After the last iteration, we will check if there is carry left
+        // If it's left then we will create a new node and add it
+        if (carry > 0) {
             temp.next = new ListNode(carry);
         }
-    return head;
+        
+        return head;
     }
 }
