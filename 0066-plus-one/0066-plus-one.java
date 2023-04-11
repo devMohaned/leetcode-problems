8cbf6ba9d71629dca925e7bab51ae3b3a57ed8ff
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2023-04-11 12:04:07 +0200

    Update 0066-plus-one.java

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,4 +1,6 @@
 class Solution {
+    // Time Complexity: O(N) where N is the size of array.
+    // Space Complexity: Avg O(1), worst O(N) where N is the size of array to be saved in memory as extra space.
     public int[] plusOne(int[] digits) {
          if (digits == null || digits.length == 0) return new int[] {1};
 
