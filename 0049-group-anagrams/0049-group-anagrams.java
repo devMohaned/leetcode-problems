70881571f4037fe0b915fc0e0fd9522e62e5883f
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2023-05-12 23:03:59 +0300

    Time: 22 ms (20.76%), Space: 47.2 MB (10.82%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList();
+        int N = strs.length;
+        Map<String, List<String>> map = new HashMap();
+      
+        for(String s : strs)
+        {
+            int[] letterCountsArr = new int[26];
+            for(char c : s.toCharArray())
+            letterCountsArr[122 - c]++;
+            
+            String key = Arrays.toString(letterCountsArr);
+            map.putIfAbsent(key, new ArrayList());
+            map.get(key).add(s);
+        }
+        result.addAll(map.values());
+        return result;
+    }
+}
