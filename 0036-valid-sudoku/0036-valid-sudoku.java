d40a6fb3182fed1f14d5365939d8697a925c9d3a
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2023-08-13 21:57:27 +0300

    Time: 15 ms (15.01%), Space: 43.9 MB (23.82%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -10,8 +10,8 @@ class Solution {
                char number = board[row][col];
                if(number == '.') continue;
                
-               boolean rowDoesNotContainsRepetition = visitedSet.add("Row #"+row +" " + number);
-               boolean colDoesNotContainsRepetition = visitedSet.add("Col #"+col +" " + number);
+               boolean rowDoesNotContainsRepetition = visitedSet.add("Row #" + row + " " + number);
+               boolean colDoesNotContainsRepetition = visitedSet.add("Col #" + col + " " + number);
                boolean subboxesDoesNotContainRepetition = visitedSet.add("Subbox " + row/3 + " " + col/3 + " " + number);
                
                if(!rowDoesNotContainsRepetition || !colDoesNotContainsRepetition || !subboxesDoesNotContainRepetition)
