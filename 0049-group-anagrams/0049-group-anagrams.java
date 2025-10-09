4b9997181fb099cf5d64a22ce1f8b2a33de93c9d
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2025-10-09 14:07:10 +0300

    Time: 18 ms (24.18%), Space: 49.7 MB (5.65%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,22 +1,18 @@
 class Solution {
-    // Space Complexity: O(N + M) where N is the size of anagramSignatures and M is the size of groups
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList();
-        Map<String, List<String>> map = new HashMap();
-        
-        // Time Complexity: O(N * M) Where N is the size of array of strings & M is the largest length of Strings found
+        Map<String, List<String>> map = new HashMap<>();
         for(String s : strs)
         {
-            int[] anagram = new int[26];
+            int[] chars = new int[26];
             for(char c : s.toCharArray())
-                anagram[122 - c]++;
-            
-            String anagramStr = Arrays.toString(anagram);
-            map.putIfAbsent(anagramStr, new ArrayList());
-            map.get(anagramStr).add(s);
+            {
+                chars[c - 'a']++;
+            }
+            String key = Arrays.toString(chars);
+            List<String> existingValues = map.getOrDefault(key,new ArrayList<>());
+            existingValues.add(s);
+            map.put(key, existingValues);
         }
-        
-        result.addAll(map.values());
-        return result;
+        return new ArrayList<>(map.values());
     }
 }
