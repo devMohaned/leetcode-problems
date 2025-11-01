381f2b4d45afc8b2490e66121c718fb45521ec1a
Mohaned Ashraf <60556571+devMohaned@users.noreply.github.com>
2025-11-01 17:07:36 +0200

    Time: 611 ms (36.19%), Space: 20.1 MB (97.09%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,25 +3,24 @@ class Solution:
         result = []
         nums.sort()
 
-
-
         for index, current in enumerate(nums):
+            if(index > 0 and current == nums[index - 1]):
+                continue
+            
             left = index + 1
             right = len(nums) - 1
-            
-            if(index > 0 and nums[index] == nums[index - 1]):
-                continue
-
             while(left < right):
-                sum = nums[left] + nums[right] + current
+                leftValue = nums[left]
+                rightValue = nums[right]
+
+                sum = current + leftValue + rightValue
                 if(sum == 0):
-                    result.append((nums[index],nums[left],nums[right]))
-                    left+=1
+                    result.append((current, leftValue, rightValue))
+                    left += 1
                     while(left < right and nums[left] == nums[left - 1]):
-                        left += 1
+                        left +=1
                 elif(sum > 0):
-                    right -=1
-                else:
-                    left+=1
+                    right -= 1
+                elif(sum < 0):
+                    left += 1
         return result
-        
